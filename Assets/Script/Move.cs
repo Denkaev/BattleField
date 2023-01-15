a52a9bc2737df3c423b7ba84c844447daf431941
Denkaev <coraa@rambler.ru>
2023-01-15 19:43:33 +1200

    Up

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody))]
 public class Move : MonoBehaviour
 {
     public float speed = 0.4f;
-    Vector2 dest = Vector2.zero;
+    Vector3 dest = Vector3.zero;
     void Start()
     {
         dest = transform.position;
@@ -14,32 +14,32 @@ public class Move : MonoBehaviour
     
     private void FixedUpdate()
     {
-        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
-        GetComponent<Rigidbody2D>().MovePosition(p);
+        Vector3 p = Vector3.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody>().MovePosition(p);
 
         // Check for Input if not moving
-        if ((Vector2)transform.position == dest)
+        if ((Vector3)transform.position == dest)
         {
-            if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
-                dest = (Vector2)transform.position + Vector2.up;
-            if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
-                dest = (Vector2)transform.position + Vector2.right;
-            if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
-                dest = (Vector2)transform.position - Vector2.up;
-            if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
-                dest = (Vector2)transform.position - Vector2.right;
+            if (Input.GetKey(KeyCode.UpArrow) && valid(Vector3.up))
+                dest = (Vector3)transform.position + Vector3.up;
+            if (Input.GetKey(KeyCode.RightArrow) && valid(Vector3.right))
+                dest = (Vector3)transform.position + Vector3.right;
+            if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector3.up))
+                dest = (Vector3)transform.position - Vector3.up;
+            if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector3.right))
+                dest = (Vector3)transform.position - Vector3.right;
         }
         
         // Animation Parameters
-        //Vector2 dir = dest - (Vector2)transform.position;
+        //Vector3 dir = dest - (Vector3)transform.position;
         //GetComponent<Animator>().SetFloat("DirX", dir.x);
         //GetComponent<Animator>().SetFloat("DirY", dir.y);
     }
 
-    bool valid(Vector2 dir)
+    bool valid(Vector3 dir)
     {
         // Cast Line from 'next to Pac-Man' to 'Pac-Man'
-        Vector2 pos = transform.position;
+        Vector3 pos = transform.position;
         RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
         return (hit.collider == GetComponent<Collider2D>());
     }
